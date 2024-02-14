d2318985785c87db4463395f00381c4a57537986
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-02-14 15:57:51 -0300

    presenters

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -87,5 +87,25 @@ describe("E2E test for customer", () => {
         expect(customer2.name).toBe("Customer 2");
         expect(customer2.address.street).toBe("Street 2");
 
+        const listResponseXML = await request(app)
+            .get("/customer")
+            .set("Accept", "application/xml")
+            .send();
+
+        expect(listResponseXML.status).toBe(200);
+        expect(listResponseXML.text).toContain(`<?xml version="1.0" encoding="UTF-8"?>`);
+        expect(listResponseXML.text).toContain(`<customers>`);
+        expect(listResponseXML.text).toContain(`<customer>`);
+        expect(listResponseXML.text).toContain(`<name>Customer 1</name>`);
+        expect(listResponseXML.text).toContain(`<address>`);
+        expect(listResponseXML.text).toContain(`<street>Street 1</street>`);
+        expect(listResponseXML.text).toContain(`<city>City 1</city>`);
+        expect(listResponseXML.text).toContain(`<number>1</number>`);
+        expect(listResponseXML.text).toContain(`<zip>12345</zip>`);
+        expect(listResponseXML.text).toContain(`</address>`);
+        expect(listResponseXML.text).toContain(`</customer>`);
+        expect(listResponseXML.text).toContain(`</customers>`);
+        
+
     });
 })
--- src/infrastructure/api/route/customer.route.ts
+++ src/infrastructure/api/route/customer.route.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import CreateCustomerUseCase from '../../../usecase/Customer/create/create.customer.usecase';
 import CustomerRepository from '../../customer/repository/sequelize/customer.repository';
 import ListCustomerUseCase from '../../../usecase/Customer/list/list.customer.usecase';
+import CustomerPresenter from '../presenters/customer.presenter';
 
 export const customerRoute = express.Router();
 
@@ -29,13 +30,17 @@ customerRoute.post('/', async (req: Request, res: Response) => {
 customerRoute.get('/', async (req: Request, res: Response) => {
     const useCase = new ListCustomerUseCase(new CustomerRepository());
 
-    try {
-        const output = await useCase.execute({});
-        res.send(output);
+    const output = await useCase.execute({});
+    res.format({
+        json: async () => res.send(output),
+        xml: async () => res.send(CustomerPresenter.listXML(output)),
+    });
 
-    } catch (err) {
-        res.status(500).send(err);
-    }
+    // try {
+    //     res.send(output);
 
+    // } catch (err) {
+    //     res.status(500).send(err);
+    // }
 
 })
