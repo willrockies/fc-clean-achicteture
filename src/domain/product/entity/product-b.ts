38c31603a0691085f0cf4dc0c5556216442b3e98
willrockies <wlsn.ads@gmail.com>
2024-02-13 21:04:40 -0300

    notification pattern em produtos

--- src/domain/product/entity/product-b.ts
+++ src/domain/product/entity/product-b.ts
@@ -1,20 +1,27 @@
+import Entity from "../../@shared/entity/entity.abstract";
+import NotificationError from "../../@shared/notification/notification.error";
 import ProductInterface from "./product.interface";
 
-export default class ProductB implements ProductInterface {
-  private _id: string;
+export default class ProductB extends Entity implements ProductInterface {
+  //private _id: string;
   private _name: string;
   private _price: number;
 
   constructor(id: string, name: string, price: number) {
+    super();
     this._id = id;
     this._name = name;
     this._price = price;
     this.validate();
+
+    if(this.notification.hasErrors()) {
+      throw new NotificationError(this.notification.getErrors());
+    }
   }
 
-  get id(): string {
-    return this._id;
-  }
+  // get id(): string {
+  //   return this._id;
+  // }
 
   get name(): string {
     return this._name;
@@ -36,10 +43,18 @@ export default class ProductB implements ProductInterface {
 
   validate(): boolean {
     if (this._id.length === 0) {
-      throw new Error("Id is required");
+      //throw new Error("Id is required");
+      this.notification.addError({
+        context: "product",
+        message: "Id is required",
+      });
     }
     if (this._name.length === 0) {
-      throw new Error("Name is required");
+      //throw new Error("Name is required");
+      this.notification.addError({
+        context: "product",
+        message: "Name is required",
+      });
     }
     if (this._price < 0) {
       throw new Error("Price must be greater than zero");
--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -5,14 +5,21 @@ describe("Product unit tests", () => {
     expect(() => {
       const product = new Product("", "Product 1", 100);
 
-    }).toThrowError("Id is required");
+    }).toThrowError("product: Id is required");
   });
 
   it("Should throw error when name is empty", () => {
     expect(() => {
       const product = new Product("123", "", 100);
 
-    }).toThrowError("Name is required");
+    }).toThrowError("product: Name is required");
+  });
+
+  it("should throw error when name and id are empty", () => {
+    expect(() => {
+      const product = new Product("", "" , 100);
+
+    }).toThrowError("product: Id is required,product: Name is required")
   });
   
   it("Should throw error when price is less than zero", () => {
